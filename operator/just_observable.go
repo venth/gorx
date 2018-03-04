102dc439908318ae2b53e423b037a6f7e3211bed
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2018-03-04 19:29:41 +0100

    corrected tests for just observable

--- operator/just_observable.go
+++ operator/just_observable.go
@@ -1,15 +1,32 @@
 package operator
 
-import "github.com/venth/gorx"
+import (
+	"errors"
+	"reflect"
+
+	"github.com/venth/gorx"
+)
 
 func Just(elements ... interface{}) gorx.Observable {
 	return CreateObservable(func(emissionObserver gorx.Observer, subscriptionState gorx.DisposableState) {
-		for element := range elements {
-			if !subscriptionState.IsDisposed() {
-				emissionObserver.OnNext(element)
+		completedWithErrors := true
+
+		for idx := 0; idx < len(elements); idx++ {
+			element := elements[idx]
+			if subscriptionState.IsDisposed() {
+				break
 			}
+
+			if reflect.ValueOf(element).IsValid() {
+				emissionObserver.OnNext(element)
+			} else {
+				emissionObserver.OnError(errors.New("nil element passed to observable.Just"))
+				completedWithErrors = false
+				break
+			}
+
 		}
-		if !subscriptionState.IsDisposed() {
+		if completedWithErrors && !subscriptionState.IsDisposed() {
 			emissionObserver.OnComplete()
 		}
 	})
--- operator/just_observable_test.go
+++ operator/just_observable_test.go
@@ -16,29 +16,104 @@ func TestObservable_Just(t *testing.T) {
 
 var _ = Describe("Observable.Just", func() {
 	var (
-		elements         []interface{}
 		justObservable   gorx.Observable
 		emissionObserver *gorx.MockObserver
 		mockCtrl         *gomock.Controller
 	)
 
 	Context("when there are no elements", func() {
-		justObservable = Just()
 
 		It("completes without emitting any element", func() {
+			justObservable = Just()
+
 			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
 			emissionObserver.EXPECT().OnError(gomock.Any()).Times(0)
 			emissionObserver.EXPECT().OnComplete().Times(1)
 
 			justObservable.Subscribe(emissionObserver)
 		})
+
+		BeforeEach(func() {
+			emissionObserver = gorx.NewMockObserver(mockCtrl)
+		})
+
+	})
+
+	Context("when there are elements", func() {
+
+		It("completes after just one element", func() {
+			element := "element"
+			justObservable = Just(element)
+
+			emissionObserver.EXPECT().OnNext(element).Times(1)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnComplete().Times(1)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		It("completes after just two elements", func() {
+			firstElement := "firstElement"
+			secondElement := "secondElement"
+			justObservable = Just(firstElement, secondElement)
+
+			observedFirstElement := emissionObserver.EXPECT().OnNext(firstElement).Times(1)
+			emissionObserver.EXPECT().OnNext(secondElement).Times(1).After(observedFirstElement)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnComplete().Times(1)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		BeforeEach(func() {
+			emissionObserver = gorx.NewMockObserver(mockCtrl)
+		})
+
+	})
+
+	Context("when passed elements are nil", func() {
+		It("emits error for nil element", func() {
+			justObservable = Just(nil)
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(1)
+			emissionObserver.EXPECT().OnComplete().Times(0)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		It("stops emission after first nil element", func() {
+			justObservable = Just(nil, nil)
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(1)
+			emissionObserver.EXPECT().OnComplete().Times(0)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		It("stops emission after first nil element, event if the next one isn't nil", func() {
+			justObservable = Just(nil, "some element")
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(1)
+			emissionObserver.EXPECT().OnComplete().Times(0)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		It("emits not nil element and stops emission on nil element", func() {
+			justObservable = Just("some element", nil)
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(1)
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(1)
+			emissionObserver.EXPECT().OnComplete().Times(0)
+
+			justObservable.Subscribe(emissionObserver)
+		})
+
+		BeforeEach(func() {
+			emissionObserver = gorx.NewMockObserver(mockCtrl)
+		})
+
 	})
 
 	mockCtrl = gomock.NewController(GinkgoT())
 	defer mockCtrl.Finish()
-
-	BeforeEach(func() {
-		elements = make([]interface{}, 0)
-		emissionObserver = gorx.NewMockObserver(mockCtrl)
-	})
 })
