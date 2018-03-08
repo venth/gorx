c807c63bbef6d319391fdf5f812a64bdac012915
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2018-03-08 20:53:41 +0100

    continued description of on error resume next operator

--- operator/on_error_resume_next_test.go
+++ operator/on_error_resume_next_test.go
@@ -18,7 +18,7 @@ func TestOperator_OnErrorResumeNext(t *testing.T) {
 var _ = Describe("Operator.OnErrorResumeNext", func() {
 	var (
 		emissionObserver *gorx.MockObserver
-		mockCtrl *gomock.Controller
+		mockCtrl         *gomock.Controller
 	)
 
 	mockCtrl = gomock.NewController(GinkgoT())
@@ -26,8 +26,10 @@ var _ = Describe("Operator.OnErrorResumeNext", func() {
 
 	Context("when emitter didn't emit any error", func() {
 		It("emits original elements", func() {
-			emissionObserver.EXPECT().OnNext("element").Times(1)
-			emissionObserver.EXPECT().OnComplete().Times(1)
+			gomock.InOrder(
+				emissionObserver.EXPECT().OnNext("element").Times(1),
+				emissionObserver.EXPECT().OnComplete().Times(1),
+			)
 
 			Just("element").OnErrorResumeNext(Just("resumed")).
 				Subscribe(emissionObserver)
@@ -52,13 +54,37 @@ var _ = Describe("Operator.OnErrorResumeNext", func() {
 	})
 
 	Context("when emitter an error", func() {
-		It("emits resumed", func() {
+		someError := errors.New("some error")
+
+		It("emits resumed observed sequence, which doesn't contain errors", func() {
 			emissionObserver.EXPECT().OnError(gomock.Any()).Times(0)
 			emissionObserver.EXPECT().OnNext("resumed").Times(1)
 			emissionObserver.EXPECT().OnComplete().Times(1)
 
-			Error(errors.New("some error")).
+			Error(someError).
 				OnErrorResumeNext(Just("resumed")).
+				Subscribe(emissionObserver)
+		})
+
+		It("emits resumed observed sequence, which contain error", func() {
+			resumedWithError := errors.New("resumed error")
+
+			emissionObserver.EXPECT().OnError(resumedWithError).Times(1)
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnComplete().Times(0)
+
+			Error(someError).
+				OnErrorResumeNext(Error(resumedWithError)).
+				Subscribe(emissionObserver)
+		})
+
+		It("completes because resumed sequence is empty", func() {
+			emissionObserver.EXPECT().OnError(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnNext(gomock.Any()).Times(0)
+			emissionObserver.EXPECT().OnComplete().Times(1)
+
+			Error(someError).
+				OnErrorResumeNext(Empty()).
 				Subscribe(emissionObserver)
 		})
 
