c9918370c70943b87b27deb85744f5a1b8f5b3b0
Artur Krysiak <artur.krysiak.warszawa@gmail.com>
2018-03-10 22:37:24 +0100

    decreased from slice cognitive complexity

--- operator/from_slice.go
+++ operator/from_slice.go
@@ -8,15 +8,19 @@ import (
 
 func FromSlice(elements interface{}) gorx.Observable {
 	return CreateObservable(func(emissionObserver gorx.Observer, subscriptionState gorx.DisposableState) {
-		listVal := reflect.ValueOf(elements)
-		completed, nilPassed := false, false
-		for i := 0; i < listVal.Len() && !completed; i++ {
-			element := listVal.Index(i).Interface()
-			completed, nilPassed = notifyJustObserver(element, subscriptionState, emissionObserver)
-		}
+		elementList := reflect.ValueOf(elements)
+		nilPassed := forEachSliceElement(elementList, subscriptionState, emissionObserver)
 
 		if !(nilPassed || subscriptionState.IsDisposed()) {
 			emissionObserver.OnComplete()
 		}
 	})
+}
+func forEachSliceElement(listVal reflect.Value, subscriptionState gorx.DisposableState, emissionObserver gorx.Observer) bool {
+	completed, nilPassed := false, false
+	for i := 0; i < listVal.Len() && !completed; i++ {
+		element := listVal.Index(i).Interface()
+		completed, nilPassed = notifyJustObserver(element, subscriptionState, emissionObserver)
+	}
+	return nilPassed
 }
